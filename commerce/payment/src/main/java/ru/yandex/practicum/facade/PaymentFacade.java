8c07bc581bfbdf9e2794c6a81187c607ed9650b2
Raisat <raisat.bekbulatova1234@gmail.ru>
2026-06-08 15:12:54 +0300

    наработки для модуля payment

---
+++ commerce/payment/src/main/java/ru/yandex/practicum/facade/PaymentFacade.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.facade;
+
+import ru.yandex.practicum.dto.order.OrderDtoPayment;
+import ru.yandex.practicum.dto.payment.PaymentDto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+/**
+ * Фасад для управления платежами
+ * Объединяет бизнес-логику создания и обработки платежей
+ */
+public interface PaymentFacade {
+    PaymentDto createPayment(OrderDtoPayment request);
+
+    BigDecimal calculateTotalPrice(OrderDtoPayment request);
+
+    BigDecimal calculateProductPrice(OrderDtoPayment request);
+
+    void handleSuccessfulPayment(UUID paymentId);
+
+    void handleFailedPayment(UUID paymentId);
+}
